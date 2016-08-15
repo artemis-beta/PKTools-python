74203b874216132f65e169418a4d49cf024e233f
artemis-beta <KXZ186@student.bham.ac.uk>
2016-08-15 20:59:03 +0100

    Added PKMatrix

---
+++ examples/Matrix_Example.py
@@ -0,0 +1,42 @@
+import sys
+sys.path.insert(0, '../source/')
+
+from PKMatrix import *
+
+a = PKMatrix()
+
+print "Construct a 3x3 matrix:\n"
+a.addRow([PKVar(12.,0.1),PKVar(15.,0.2),PKVar(11.,0.3)])
+a.addRow([PKVar(5.,0.2),PKVar(7.,0.1),PKVar(9.,0.3)])
+a.addRow([PKVar(10.,0.1),PKVar(5.,0.1),PKVar(8.,0.1)])
+
+a.Print()
+
+print "Construct another 3x3 matrix:\n"
+b = PKMatrix()
+b.addRow([PKVar(2.,0.1),PKVar(1.,0.2),PKVar(1.,0.3)])
+b.addRow([PKVar(6.,0.2),PKVar(5.,0.1),PKVar(3.,0.3)])
+b.addRow([PKVar(0.,0.1),PKVar(2.,0.1),PKVar(1.,0.1)])
+
+b.Print()
+
+print "Add them together:\n"
+c = a+b
+c.Print()
+print "Multiply them together:\n"
+d = a*b
+d.Print()
+print "Take the Transpose:\n"
+c.Transpose().Print()
+print "The Trace is:\n"
+c.Trace().Print()
+print "\nLets Try a 2x3 matrix\n"
+d = PKMatrix()
+d.addRow([PKVar(1.4,0.2),PKVar(5.8,0.2)])
+d.addRow([PKVar(3.2,0.1),PKVar(1.5,0.4)])
+d.addRow([PKVar(6.0,0.3),PKVar(5.6,0.1)])
+d.Print()
+print "Take the Transpose:\n"
+d.Transpose().Print()
+print "Can't do the Trace!\n"
+d.Trace()
---
+++ source/PKMatrix.py
@@ -0,0 +1,86 @@
+from PKVar import *
+
+class PKMatrix:
+	def __init__(self,rows_columns=None):
+		if rows_columns is None:
+			rows_columns = []
+		self.elements = rows_columns
+		self.outstring = ""
+	def addRow(self,row):
+		self.elements.append(row)
+	def addColumn(self,column):
+		for i in range(0,len(self.elements)-1):
+			self.elements[i].append(column[i])
+	def __str__(self):
+		for i in range(0,len(self.elements)):
+			print "(",
+			for j in range(0,len(self.elements[0])):
+				print self.elements[i][j].returnString(),
+			print ")"	
+	def __add__(self,param):
+		if(len(param.elements) is not len(self.elements) and len(param.elements[0]) is not len(self.elements[0])):
+			print "ERROR: Cannot add matrices of varying dimensions\n"
+			return PKMatrix()
+		new_list = []
+		for i, k in zip(self.elements, param.elements):
+			new_list.append([])
+			for j, l in zip(i, k):
+				new_list[-1].append(j+l)
+	
+		temp = PKMatrix(new_list)
+		return temp
+
+	def __sub__(self,param):
+		
+		if(len(param.elements) is not len(self.elements) and len(param.elements[0]) is not len(self.elements[0])):
+			print "ERROR: Cannot subtract matrices of varying dimensions\n"
+			return PKMatrix()
+		new_list = []
+		for i, k in zip(self.elements, param.elements):
+			new_list.append([])
+			for j, l in zip(i, k):
+				new_list[-1].append(j-l)
+	
+		temp = PKMatrix(new_list)
+		return temp
+
+	def Transpose(self):
+			
+		new_list = []
+		for i in range(0,len(self.elements[0])):
+			new_list.append([])
+			for j in range(0,len(self.elements)):
+				new_list[i].append(self.elements[j][i])
+		temp = PKMatrix(new_list)	
+
+		return temp
+
+	def __mul__(self,param):
+	
+		if(len(param.elements) is not len(self.elements[0])):
+			print "ERROR: cannot multiply matrices with set dimensions\n"
+			return PKMatrix()
+		tranpose_param = param.Transpose()
+		new_list = []
+		for i in range(0,len(self.elements)):
+			new_list.append([])
+			for j in range(0,len(self.elements[0])):
+				new_list[-1].append(PKVar(0,0))
+				for l in range(0,len(self.elements)):
+					new_list[-1][-1] = new_list[-1][-1] + self.elements[i][l]*param.elements[l][j]
+
+		temp = PKMatrix(new_list)
+		return temp
+
+	def Trace(self):
+		x = PKVar(0,0)
+		if(len(self.elements[0]) is not len(self.elements)):
+			print "ERROR: Trace can only be calculated for a square matrix!\n"
+			return PKVar(0,0)
+
+		for i in range(0,len(self.elements)):
+			x = x + self.elements[i][i]
+		return x
+
+	def Print(self):
+		return self.__str__()	
