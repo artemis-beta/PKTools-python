dc1992e7e6e45e6a25629dd22459d8bf048acd72
artemis-beta <KXZ186@student.bham.ac.uk>
2016-08-17 21:22:29 +0100

    Added PKTriangle

---
+++ examples/PKTriangle_example.py
@@ -0,0 +1,11 @@
+import sys
+sys.path.insert(0, '../source/')
+
+from PKTriangle import *
+
+triangle = PKTriangle(3,3,(4./3.)*atan(1),"SSA")
+print("Triangle 1:\t%s" % triangle)
+triangle2 = PKTriangle(2*atan(1),(4./3.)*atan(1),15.,"AAS")
+print("Triangle 2:\t%s" % triangle2)
+print("Is Right Angled?")
+print triangle2.isRightAngled()
