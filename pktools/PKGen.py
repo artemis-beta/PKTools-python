07f028f4179d2ea04bfe926735ba0f90b8c92174
artemis-beta <KXZ186@student.bham.ac.uk>
2019-02-06 22:32:54 +0000

    Added options to plot and store data

--- pktools/PKGen.py
+++ pktools/PKGen.py
@@ -1,9 +1,9 @@
 import scipy.interpolate as interp
 import scipy.integrate as integ
-
 import numpy as np
-
 import logging
+import matplotlib.pyplot as plt
+import yaml
 
 logging.basicConfig()
 
@@ -39,7 +39,30 @@ class PKGen(object):
         cum_integ = integ.cumtrapz(y,x)
         return interp.interp1d(cum_integ, x[:-1], bounds_error=False)
 
-    def generate(self, nevts):
-        '''Generate 'nevts' events using the PDF'''
+    def generate(self, nevts, show_plot=100, save_data=False):
+        '''
+        Generate 'nevts' events using the PDF
+
+        Arguments
+        ---------
+
+        nevts     (int)        Number of events to generate
+
+        Optional Arguments
+        ------------------
+
+        show_plot   (int)      Either an integer or None depending on
+                               whether you wish the results to be plotted
+                               as a histogram
+
+        save_data   (string)   Either a string representing the output yaml file
+                               address or False if you do not want to save the data
+        '''
         _data_set = [self._gen_func(np.random.uniform()) for i in range(nevts)]
-        return np.array(_data_set)
+        _data_set = np.array(_data_set)
+
+        if show_plot:
+            plt.hist(_data_set, bins=show_plot)
+
+        if save_data:
+            yaml.dump(_data_set, open(save_data, 'w'))
